ece807100306859503c893f6c7bb4c086855df48
Ivan <ilushinvanya@gmail.com>
2017-06-03 16:04:02 +0300

    commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        location: window.location
+
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    mounted: function(){
+
+    }
+});
+
+
